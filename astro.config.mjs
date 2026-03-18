13e92be84a3cc9ce2d7fb86f2d96faf175b4b33a
kiritan-hugo <sdgb82@gmail.com>
2026-03-18 09:19:43 +0900

    20260318-2-verselinstall

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://andes-life.com',
-    integrations: [mdx(), sitemap(), react(), sitemap()],
+  site: 'https://andes-life.com',
+  integrations: [mdx(), sitemap(), react(), sitemap()],
+  adapter: vercel(),
 });
